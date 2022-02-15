22f96309200ea8234c8ea238168944a0d7dff9f7
Geovanna Favilla <geovanna.favilla@gmail.com>
2022-02-14 22:56:00 -0300

    mudanças

--- codigo/ex6.R
+++ codigo/ex6.R
@@ -1,10 +1,10 @@
 library(qcc) #gráficos de controle de qualidade
 library(readxl)
 
-planilha <- read_excel("./dados/exercicio6.xls")
+ex6 <- read_excel("./dados/exercicio6.xls")
 
-pessoas <- planilha$'Nº pessoas'
-names(pessoas) <- planilha$'Qualidade'
+pessoas <- ex6$'Nº pessoas'
+names(pessoas) <- ex6$'Qualidade'
 #Criação do gráfico pareto
 png(filename = "./graficos/ex6-fig1.png", width = 800, height = 600)
 graficoPareto <- pareto.chart(pessoas, main="Exercício 6 - Qualificação de Atendimento", ylab2="Frequência Cumulativa", ylab="Frequência", cumperc = seq(0, 100, by = 25), col = rainbow(8))
